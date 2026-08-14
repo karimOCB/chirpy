8478a8e4324bd5274c2b2cbbdeac11affffa79a2
Karim <chaya.karim.omar@gmail.com>
2026-08-14 19:26:56 +0000

    AB: Server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux() 
+
+	server := http.Server{
+		Handler: mux,
+		Addr: ":8080",
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("couldn't listen to the server: %v", err)
+	}
 }
+
