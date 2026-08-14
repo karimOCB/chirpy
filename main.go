7cfcabee82049f0f76eaf8269ba7624da141f556
Karim <chaya.karim.omar@gmail.com>
2026-08-14 19:53:03 +0000

    AC: Server 0.2

--- main.go
+++ main.go
@@ -6,15 +6,20 @@ import (
 )
 
 func main() {
+	port := "8080"
+
 	mux := http.NewServeMux() 
 
-	server := http.Server{
+	server := &http.Server{
 		Handler: mux,
-		Addr: ":8080",
+		Addr: ":" + port,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	filepathRoot := http.Dir(".")
+	mux.Handle("/", http.FileServer(filepathRoot))
 
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("couldn't listen to the server: %v", err)
