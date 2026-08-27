9a32e05c94f5905ac206737b9dc58f64faf2b518
Karim <chaya.karim.omar@gmail.com>
2026-08-27 23:58:00 +0000

    AN: JWT v 0.1

---
+++ internal/auth/JWT.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+			Issuer: "chirpy-access",
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+			Subject: userID.String(),
+		},
+		// TODO: jwt.Token. []byte(tokenSecret)
+	)
+
+}
